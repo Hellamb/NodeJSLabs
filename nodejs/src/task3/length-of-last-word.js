eb6b259f5f171f08ef19a47495cc91ee04c9af90
Vladyslav-Kuksiuk <vladyslav.kuksiuk@teamdev.com>
2023-03-30 13:50:17 +0300

    Implement task3 in lab1.

---
+++ nodejs/src/task3/length-of-last-word.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLastWord = function (s) {
+    const arr = s.trim().split(' ');
+    const lastWord = arr[arr.length - 1]
+    return lastWord.length;
+};
+
+const string = 'Hello World, Hello NodeJS';
+console.log(lengthOfLastWord(string))
